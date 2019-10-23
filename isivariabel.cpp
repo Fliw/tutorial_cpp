f9d4544cec8b8b7fa77e12293cfb0cd1484d2af5
Fliw <fliw@digitalkode.com>
2019-10-23 14:22:01 +0700

    output dan penempatan isi variabel

---
+++ isivariabel.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ //int main wajib dalam sebuah codingan cpp, dia sebagai "body" utama c++
+int main()
+{
+  //disini saya deklarasikan sebuah variabel dengan nama X
+  string x;
+  //disini saya isi variabel x dengan sebuah string
+  x = "Halo variabel";
+  //disini saya output kan nilai dari variabel x
+  cout<<x; 
+}
