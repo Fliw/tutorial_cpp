773a2012fcf6d2bbba403b526132e70db2fb08fd
Fliw <fliw@digitalkode.com>
2019-10-23 14:06:29 +0700

    output dan penempatan isi variabel

---
+++ variabel.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ //int main wajib dalam sebuah codingan cpp, dia sebagai "body" utama c++
+int main()
+{
+    //disini saya membuat sebuah variabel X dengan tipe data sebagai integer(integer adalah tipe data bilangan bulat)
+    int x;
+    //disini saya mengisi variabel X Dengan Sebuah Kalimat Haloo Variabel
+    x = 18;
+    //dalam cpp untuk meng output kan sebuah variabel, diperlukan cout<< yang didalamnya kita tulis nama variabel kita (X)
+    cout<<"umur saya adalah "<<x;
+    return 0;
+}
