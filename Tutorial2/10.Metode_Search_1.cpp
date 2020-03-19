c40b322604bda2854e878c0af23f726a50c29ea6
fliw <fliw@digitalkode.com>
2020-03-19 22:57:03 +0700

    Nambah Komen

--- Tutorial2/10.Metode_Search_1.cpp
+++ Tutorial2/10.Metode_Search_1.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
-
 using namespace std;
 int main(){
+    //membuat array berisi banyak data acak
     int larik[15]={23,1,5,4,8,2,6,2,8,9,32,5,9,1,99};
     int i,x,posisi;
     cout<<"Data Yang Ingin Anda Cari = ";
     cin>>x;
     i = 0;
     posisi=0;
+    //looping selama si i kurang dri 15 dan larik i tidak sama dengan permintaan user
     while (i<15 && larik[i]!=x)
     {
         i++;
     }
+    //setelah di looping dan masih gk ketemu
     if(larik[i]!=x){
         cout<<"data yang anda temukan tidak ada";
     }
+    //setelah looping dan ketemu
     else if(larik[i]==x){
+            //assign posisi saat ini + 1
             posisi=i+1;
             cout<<"ketemu donk pada posisi "<<posisi;
         }
--- Tutorial2/11.Metode_Search_2.cpp
+++ Tutorial2/11.Metode_Search_2.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
+//membuat konstanta
 #define true 1
 #define false 0
 using namespace std;
 int main(){
+    //membuat array yang sudah berurutan
     int larik[]={1,2,3,4,5,6,7,8,9,10,11,12};
     int i=0,ia=0,ib=12,k,x;
     bool ketemu;
     cout<<"Masukkan Data Yang Akan Dicari !";
     cin>>x;
+    //ketika awal ketemu masih false
     ketemu=false;
+    //kita potong menjadi pertengahan diantara banyaknya array
     k=(ia+ib)/2;
+    //looping teroooss
     while (!ketemu&&ia<=ib)
     {
+        //kalau larik k cocok sama x maka ketemu true
         if(larik[k]==x){
             ketemu=true;
         }
+        //klo nggk dia bakal geser ke kiri / kanan tergantung besar kecilnya inputan
         else{
             if(larik[k]<x){
                 k=k+1;
@@ -24,9 +31,11 @@ int main(){
             }
         }
     }
+    //kalau ketemu
     if(ketemu){
         cout<<"data ditemukan pada posisi = "<<k+1;
     }
+    //kalau nggak
     else {
         cout<<"data tidak ditemukan !";
     }
