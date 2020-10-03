f381a9fb65ce7e68f573bceb02f68594edfcf3e6
anssanbla <anssanbla16@gmail.com>
2020-10-03 11:19:50 +0700

    Tambah Variabel Integer

--- Tutorial/2.AssignVariabel.cpp
+++ Tutorial/2.AssignVariabel.cpp
@@ -5,8 +5,11 @@ int main()
 {
   //disini saya deklarasikan sebuah variabel dengan nama X
   string x;
+  int angka;
   //disini saya isi variabel x dengan sebuah string
   x = "Halo variabel";
+  angka = 5;
   //disini saya output kan nilai dari variabel x
   cout<<x; 
+  cout<<angka;
 }
